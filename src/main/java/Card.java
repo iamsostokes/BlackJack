26159cdc4a923a8f5600d4ba1eb7aa4b6f6a2c01
Jamez Stokes <jamezstokes@MacBook-Pro-7.local>
2019-02-18 14:07:19 -0500

    I created card and deck builder

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,93 @@
 public class Card {
+
+    private String suit;
+    private String rank;
+    private int weight;
+
+
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+        this.weight = weightOfCard(rank);
+    }
+
+    public int weightOfCard(String rank) {
+        int card = 0;
+
+        if (rank.equalsIgnoreCase("ace")) {
+            card = 1;
+
+        } else if (rank.equalsIgnoreCase("2")) {
+            card = 2;
+
+        } else if (rank.equalsIgnoreCase("3")) {
+            card = 3;
+
+        } else if (rank.equalsIgnoreCase("4")) {
+            card = 4;
+
+        } else if (rank.equalsIgnoreCase("5")) {
+            card = 5;
+
+        } else if (rank.equalsIgnoreCase("6")) {
+            card = 6;
+
+        } else if (rank.equalsIgnoreCase("7")) {
+            card = 7;
+
+        } else if (rank.equalsIgnoreCase("8")) {
+            card = 8;
+
+        } else if (rank.equalsIgnoreCase("9")) {
+            card = 9;
+
+        } else if (rank.equalsIgnoreCase("10")) {
+            card = 10;
+
+        } else if (rank.equalsIgnoreCase("jack")) {
+            card = 11;
+
+        } else if (rank.equalsIgnoreCase("queen")) {
+            card = 12;
+
+        } else if (rank.equalsIgnoreCase("king")) {
+            card = 13;
+
+        }
+        return card;
+    }
+
+    public void printCardDetails() {
+        System.out.println(getRank() + " of " + getSuit() + "\n");
+    }
+
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
+
+
+
+
+
--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Deck {
+
+    private ArrayList<Card> deck = new ArrayList<>();
+
+    public Deck(){
+        deckBuilder();
+    }
+
+    private void deckBuilder(){
+        ArrayList<String> suits = new ArrayList<>(Arrays.asList("Heart", "Clubs", "Spades", "Diamonds"));
+        ArrayList<String> ranks = new ArrayList<>(Arrays.asList("Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"));
+
+        for (String suit: suits) {
+            for (String rank: ranks) {
+                Card card = new Card(suit, rank);
+                deck.add(card);
+            }
+        }
+
+        System.out.println(deck.size());
+
+    }
+
+
 }
